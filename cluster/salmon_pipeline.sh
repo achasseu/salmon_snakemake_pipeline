3a38673bf3c736b80f6977d2016198e0cffa0ce5
Alexis Chasseur <79086502+achasseu@users.noreply.github.com>
2026-06-25 13:38:14 +0200

    Update salmon_pipeline.sh

--- cluster/salmon_pipeline.sh
+++ cluster/salmon_pipeline.sh
@@ -2,6 +2,6 @@
 
 snakemake \
     --jobs 50 \
-    --cluster "sbatch -A sens2020530 -p core -t 10-00:00:00 -n 1" \
+    --cluster "sbatch -A [PROJECT NAME] -p core -t 10-00:00:00 -n 1" \
     --rerun-incomplete \
     --latency-wait 60
